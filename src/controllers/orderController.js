b4a6d0076c36a5d780c74f00af70058faefa53f8
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-05-30 16:32:23 +0200

    comenzando panel administrador

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,21 +1,55 @@
-const {Order, ListaJuegos} = require('../models/indexModels');
+const {Order, ListaJuegos, Games} = require('../models/indexModels');
 const {  prevenirInyeccionCodigo, catchAsync, response, gestionErrores, ClientError, generarHashpass, calcularPrecio } = require('../utils/indexUtils');
-
+const mongoose = require('mongoose');
 // crear usuario
 const postCrearOrder = async (req, res) => {
-
+    const listaJuegosBody=req.body.listaJuegos
     const newOrder=new Order({
         userId: prevenirInyeccionCodigo(req.body.userId),
-        listaJuegos: req.body.listaJuegos,
+        listaJuegos: listaJuegosBody,
         date: new Date(),
         address: req.body.address,
         state:'Pagado',
         totalPrice: req.body.totalPrice,
         })
-    // Guardar el pedido en la base de datos
-    const savedOrder = await newOrder.save();
+        const session = await mongoose.startSession();
+        session.startTransaction();
+    
+        try {
+            // Iterar sobre cada juego en la lista para actualizar el stock
+            for (const x of listaJuegosBody) {
+                const updatedGame = await Games.findOneAndUpdate(
+                    { _id: x.gameId },
+                    { $inc: { stock: -x.quantity } },
+                    { new: true, runValidators: true, session }
+                );
+    
+                if (!updatedGame) {
+                    throw new ClientError(`Juego con ID ${x.gameId} no encontrado`, 404);
+                }
+    
+                if (updatedGame.stock < 0) {
+                    throw new ClientError(`No hay suficiente stock para el juego ${updatedGame.name}`, 403);
+                }
+            }
+    
+            // Guardar el pedido en la base de datos
+            const savedOrder = await newOrder.save({ session });
+    
+            // Confirmar la transacción
+            await session.commitTransaction();
+            session.endSession();
+
+            // Enviar el pedido guardado como respuesta
+            response(res, 200, savedOrder)
+        } catch (error) {
+            // Abortar la transacción en caso de error
+            await session.abortTransaction();
+            session.endSession();
+            throw new ClientError(`No se ha podido realizar el pedido: ${error}`, 500)
+        }
     // Enviar el usuario guardado como respuesta
-    response(res, 200, savedOrder)
+    
 }
 //recoge todos los usuarios
 const getOrder= async (req,res)=>{
